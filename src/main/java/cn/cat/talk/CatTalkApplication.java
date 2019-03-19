c5764c6f4eb99a09e9a68856aa80221a6fbcb6d8
wangzun <1125334796@qq.com>
2019-03-19 16:06:24 +0800

    init project

---
+++ src/main/java/cn/cat/talk/CatTalkApplication.java
@@ -0,0 +1,13 @@
+package cn.cat.talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/cat/talk/CatTalkApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cat.talk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatTalkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
